6cc82a7a1467cdcc405e8beaa4e8351066d77b10
Kyle Ledbetter <kyle@kyleledbetter.com>
2017-02-01 12:19:29 -0600

    Revert "seed mock charts from yaml"

--- http/handlers/charts.go
+++ http/handlers/charts.go
@@ -9,6 +9,20 @@ import (
 	"github.com/julienschmidt/httprouter"
 )
 
+type Chart struct {
+	Name          string  `json:"name"`
+	Key           string  `json:"key"`
+	NumDataPoints float64 `json:"num_data_points"`
+	IntervalS     float64 `json:"interval_seconds"`
+	Active        bool    `json:"active"`
+	YAxes         []YAxis `json:"y_axes"`
+}
+
+type YAxis struct {
+	Name         string `json:"name"`
+	FunctionType string `json:"function_type"`
+}
+
 func ReadChart(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	key := ps.ByName("key")
 	log.Info("Retrieving chart data for ", key)
@@ -24,7 +38,49 @@ func ReadChart(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 }
 
 func GetCharts(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	log.Info("Retrieving list of charts")
+	log.Info("Executing ListSets()")
 	data := charts.ListSets()
 	helpers.Respond(w, data, http.StatusOK)
+}
+
+func CreateChart(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	log.Info("Executing Create New Chart")
+	c := &Chart{}
+	err := helpers.GetRequestBody(r, c)
+	if err != nil {
+		log.Error("Malformed request: ", err)
+		helpers.Respond(w, err, http.StatusBadRequest)
+		return
+	}
+	// TODO: check y axis function exists
+
+	// create new chart
+	s := charts.NewSet(c.Name, c.Key, int(c.NumDataPoints), int(c.IntervalS))
+	if s == nil {
+		log.Error("Malformed request: Length or Interval")
+		// TODO fix the error stuff to accept variadic status
+		helpers.Respond(w, "Malformed request: Length or Interval missing.", http.StatusBadRequest)
+		return
+	}
+	// add y axes
+	for _, y := range c.YAxes {
+		err := s.AddYAxis(y.Name, charts.Functions[y.FunctionType])
+		if err != nil {
+			helpers.Respond(w, "Malformed request: Function does not exist", http.StatusBadRequest)
+			return
+		}
+	}
+
+	// start it
+	s.Run()
+	c.Active = s.IsActive()
+	c.Key = s.Key
+
+	helpers.Respond(w, c, http.StatusOK)
+}
+
+func DeleteChart(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	key := ps.ByName("key")
+	charts.DeleteSet(key)
+	helpers.Respond(w, nil, http.StatusOK)
 }
