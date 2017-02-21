f176fb4b965dde7c9f7abb34c8cd6b08178c74f4
ilsiepotamus <ilsun.park@gmail.com>
2017-02-21 14:55:15 -0600

    add standard claims to token response

--- login/token.go
+++ login/token.go
@@ -1,49 +1,64 @@
 package login
 
 import (
-	//"time"
 	log "github.com/Sirupsen/logrus"
 	jwt "github.com/dgrijalva/jwt-go"
+	"time"
 )
 
 type UserClaims struct {
-	Foo string `json:"foo"`
 	jwt.StandardClaims
 }
 
 func RequestToken(user *map[string]interface{}) (*map[string]interface{}, error) {
-	userData, err := Authenticate(user)
+	_, err := Authenticate(user)
 	if err != nil {
 		return nil, err
 	}
+
+	u := *user
+	userToken := map[string]interface{}{}
+	userToken["username"] = u["username"].(string)
 
 	log.Info("Generating JWT token...")
-	token := generateToken(userData)
+
+	// TODO: change 7200 (2 hours) to variable
+	t := generateToken(&userToken, 7200)
 	if err != nil {
 		return nil, err
 	}
 
-	u := *userData
-	u["access_token"] = token
-	u["token_type"] = "Bearer"
-	return &u, nil
+	return t, nil
 }
 
-func generateToken(user *map[string]interface{}) string {
+func generateToken(user *map[string]interface{}, expires int64) *map[string]interface{} {
 	key := []byte("AllYourBase")
+	now := time.Now().Unix()
+	u := *user
 
 	// Create the Claims
 	claims := UserClaims{
-		"bar",
 		jwt.StandardClaims{
-			ExpiresAt: 15000,
+			ExpiresAt: now + expires,
 			Issuer:    "jwt overlord",
+			Id:        u["username"].(string),
+			IssuedAt:  now,
+			NotBefore: now,
 		},
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	ss, _ := token.SignedString(key)
-	return ss
+
+	u["expires_at"] = claims.ExpiresAt
+	u["issuer"] = claims.Issuer
+	u["id"] = claims.Id
+	u["issed_at"] = claims.IssuedAt
+	u["not_before"] = claims.NotBefore
+	u["access_token"] = ss
+	u["token_type"] = "Bearer"
+
+	return &u
 }
 
 func remapUserObject(user *map[string]interface{}) *map[string]interface{} {
