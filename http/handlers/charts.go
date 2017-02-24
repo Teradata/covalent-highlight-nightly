01330af67fe49ca0ccf8a5d8b9ab3c31df84ae16
Ilsun Park <ilsun.park@gmail.com>
2017-02-23 18:47:12 -0600

    Feature/go 1.8 upgrade (#28)
    * Vendoring dependencies
    
    * Replace httprouter with hi
    
    * add Godep vendor dependencies for heroku
    
    * update travis build file with go 1.8

--- http/handlers/charts.go
+++ http/handlers/charts.go
@@ -6,11 +6,11 @@ import (
 	log "github.com/Sirupsen/logrus"
 	"github.com/Teradata/covalent-data/charts"
 	"github.com/Teradata/covalent-data/http/helpers"
-	"github.com/julienschmidt/httprouter"
+	"github.com/pressly/chi"
 )
 
-func ReadChart(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	key := ps.ByName("key")
+func ReadChart(w http.ResponseWriter, r *http.Request) {
+	key := chi.URLParam(r, "key")
 	log.Info("Retrieving chart data for ", key)
 	params := helpers.GetQueryParams(r)
 	data, total := charts.GetSetData(key, params.PerPage)
@@ -23,7 +23,7 @@ func ReadChart(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	helpers.Respond(w, data, http.StatusOK)
 }
 
-func GetCharts(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+func GetCharts(w http.ResponseWriter, r *http.Request) {
 	log.Info("Retrieving list of charts")
 	data := charts.ListSets()
 	helpers.Respond(w, data, http.StatusOK)
